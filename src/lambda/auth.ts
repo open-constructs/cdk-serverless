470e9d6fd405db42408ffa8cab45ef6bc8d50ff4
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2023-02-18 01:40:40 +0100

    chore: comments

--- src/lambda/auth.ts
+++ src/lambda/auth.ts
@@ -68,22 +68,42 @@ const promisedVerify = (token: string): Promise<{ [name: string]: string }> => {
   });
 };
 
+/**
+ * CognitoAuthorizer is an abstract class representing an authorizer
+ * that can be used to authenticate and authorize a user through Cognito.
+ */
 export abstract class CognitoAuthorizer {
 
+  /**
+   * The claims of the authenticated user.
+   */
   protected claims?: { [name: string]: string | number | boolean | string[] };
 
+  /**
+   * Authenticates the user and sets the claims.
+   */
   public abstract authenticate(): Promise<void>;
 
+  /**
+   * Returns true if the user is authenticated, false otherwise.
+   */
   public isAuthenticated(): boolean {
     return this.claims !== undefined;
   }
 
+  /**
+   * Throws an UnauthenticatedError if the user is not authenticated.
+   */
   public assertAuthenticated(): void {
     if (!this.isAuthenticated()) {
       throw new UnauthenticatedError();
     }
   }
 
+  /**
+   * Returns an array of groups the user belongs to, or an empty array if the user
+   * is not authenticated or has no groups.
+   */
   public getGroups(): string[] {
     if (!this.isAuthenticated() || !this.claims!.hasOwnProperty('cognito:groups')) {
       return [];
@@ -91,11 +111,20 @@ export abstract class CognitoAuthorizer {
     return this.claims!['cognito:groups'] as unknown as string[];
   }
 
+  /**
+   * Returns true if the user belongs to the specified group, false otherwise.
+   * @param group The name of the group to check.
+   */
   public hasGroup(group: string): boolean {
     // 'cognito:groups': [ 'admin' ],
     return this.getGroups().includes(group);
   }
 
+  /**
+   * Throws a ForbiddenError if the user is not authenticated or does not belong
+   * to the specified group.
+   * @param group The name of the group to check.
+   */
   public assertGroup(group: string): void {
     this.assertAuthenticated();
     if (!this.hasGroup(group)) {
@@ -103,47 +132,82 @@ export abstract class CognitoAuthorizer {
     }
   }
 
+  /**
+   * Returns true if the user belongs to the 'admin' group, false otherwise.
+   */
   public isAdmin(): boolean {
     return this.hasGroup('admin');
   }
 
+  /**
+   * Throws a ForbiddenError if the user is not authenticated or is not an admin.
+   */
   public assertAdmin(): void {
     this.assertGroup('admin');
   }
 
+  /**
+   * Returns the email of the authenticated user.
+   */
   public getEMail(): string {
     this.assertAuthenticated();
     return this.claims!.email as string;
   }
 
+  /**
+   * Returns the subject of the authenticated user.
+   */
   public getSubject(): string {
     this.assertAuthenticated();
     return this.claims!.sub as string;
   }
 
+  /**
+   * Returns the value of the claim with the specified name, or undefined if the
+   * claim does not exist.
+   * @param name The name of the claim to retrieve.
+   */
   public getClaim(name: string): string | number | boolean | string[] | undefined {
     this.assertAuthenticated();
     return this.claims![name];
   }
 }
 
+/**
+ * ApiGatewayv1CognitoAuthorizer is a class that extends CognitoAuthorizer
+ * and implements authentication logic for API Gateway v1 requests with a
+ * Cognito authorizer.
+ */
 export class ApiGatewayv1CognitoAuthorizer extends CognitoAuthorizer {
 
+  /**
+   * The event that triggered the authorization check.
+   */
   constructor(protected event: AWSLambda.APIGatewayProxyWithCognitoAuthorizerEvent, private _logger: logger.LambdaLog) {
     super();
   }
 
+  /**
+   * Authenticates the user using the claims provided by the API Gateway event or
+   * by decoding a JWT token from the 'Authorization' header.
+   */
   public async authenticate(): Promise<void> {
+    // Try to get the claims from the event
     this.claims = this.event.requestContext.authorizer?.claims;
 
+    // If no pool ID is defined or claims exist, return early
     if (!cognitoPoolId || this.claims) {
       return;
     }
+
+    // Extract the token from the Authorization header
     const authHeader = this.event.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return;
     }
     const token = authHeader.substring('Bearer '.length);
+
+    // Verify the token and set the claims on the event and this object
     try {
       const claims: { [name: string]: string } = await promisedVerify(token);
       this._logger.debug(JSON.stringify(claims));
@@ -157,24 +221,41 @@ export class ApiGatewayv1CognitoAuthorizer extends CognitoAuthorizer {
 
 }
 
-
+/**
+ * ApiGatewayv2CognitoAuthorizer is a class that extends CognitoAuthorizer
+ * and implements authentication logic for API Gateway v2 requests with a
+ * JWT authorizer.
+ */
 export class ApiGatewayv2CognitoAuthorizer extends CognitoAuthorizer {
 
+  /**
+   * The event that triggered the authorization check.
+   */
   constructor(protected event: AWSLambda.APIGatewayProxyEventV2WithJWTAuthorizer, private _logger: logger.LambdaLog) {
     super();
   }
 
+  /**
+   * Authenticates the user using the claims provided by the API Gateway event or
+   * by decoding a JWT token from the 'Authorization' header.
+   */
   public async authenticate(): Promise<void> {
+    // Try to get the claims from the event
     this.claims = this.event.requestContext.authorizer?.jwt?.claims;
 
+    // If no pool ID is defined or claims exist, return early
     if (!cognitoPoolId || this.claims) {
       return;
     }
+
+    // Extract the token from the Authorization header
     const authHeader = this.event.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return;
     }
     const token = authHeader.substring('Bearer '.length);
+
+    // Verify the token and set the claims on the event and this object
     try {
       const claims: { [name: string]: string } = await promisedVerify(token);
       this._logger.debug(JSON.stringify(claims));
@@ -195,13 +276,25 @@ export class ApiGatewayv2CognitoAuthorizer extends CognitoAuthorizer {
 
 }
 
+/**
+ * AppSyncCognitoAuthorizer is a class that extends CognitoAuthorizer
+ * and implements authentication logic for AppSync resolver events with a
+ * Cognito authorizer.
+ */
 export class AppSyncCognitoAuthorizer extends CognitoAuthorizer {
 
+  /**
+   * The event that triggered the authorization check.
+   */
   constructor(protected event: AWSLambda.AppSyncResolverEvent<any>) {
     super();
   }
 
+  /**
+   * Authenticates the user using the claims provided by the AppSync event.
+   */
   public async authenticate(): Promise<void> {
+    // Try to get the claims from the event
     if (this.event.identity) {
       this.claims = (this.event.identity as AWSLambda.AppSyncIdentityCognito).claims;
     }
--- src/lambda/handler.ts
+++ src/lambda/handler.ts
@@ -7,12 +7,18 @@ import * as errors from './errors';
 /// HTTP Api
 /////////////////////////////////
 
+/**
+ * An object containing the properties for an HTTP response.
+ */
 export interface HttpResponseContext {
   statusCode?: number;
   headers: { [name: string]: string };
   json: boolean;
 }
 
+/**
+ * An object containing the properties for an HTTP request handler.
+ */
 export interface HttpHandlerContext {
   event: AWSLambda.APIGatewayProxyWithCognitoAuthorizerEvent;
   lambdaContext: AWSLambda.Context;
@@ -21,11 +27,17 @@ export interface HttpHandlerContext {
   cognitoAuth: CognitoAuthorizer;
 }
 
+/**
+ * A type definition for an HTTP handler.
+ */
 export type HttpHandler<T, R> = (
   context: HttpHandlerContext,
   body: T,
 ) => Promise<R>;
 
+/**
+ * An object containing the properties for an OpenAPI operation.
+ */
 export interface Operation {
   responses: {
     [statusCode: number]: {
@@ -36,28 +48,47 @@ export interface Operation {
   };
 }
 
+/**
+ * A type definition for an API Gateway v1 request handler.
+ */
 export type APIGatewayv1Handler =
   AWSLambda.Handler<AWSLambda.APIGatewayProxyWithCognitoAuthorizerEvent, AWSLambda.APIGatewayProxyResult | undefined>;
 
+/**
+ * An object containing the properties for an OpenAPI operation with a request body.
+ */
 export interface OperationWithRequestBody extends Operation {
   requestBody: { content: { 'application/json': any } };
 }
 
+/**
+ * A factory function that creates an HTTP request handler for an OpenAPI operation with a request body.
+ */
 export const createOpenApiHandlerWithRequestBody = <OP extends OperationWithRequestBody, SC extends number = 200>(handler: HttpHandler<OP['requestBody']['content']['application/json'], OP['responses'][SC]['content']['application/json']>): APIGatewayv1Handler => {
   return createHttpHandler(handler);
 };
 
+/**
+ * A factory function that creates an HTTP request handler for an OpenAPI operation with a request body and no response.
+ */
 export const createOpenApiHandlerWithRequestBodyNoResponse = <OP extends OperationWithRequestBody>(handler: HttpHandler<OP['requestBody']['content']['application/json'], void>): APIGatewayv1Handler => {
   return createHttpHandler(handler);
 };
 
+/**
+ * A factory function that creates an HTTP request handler for an OpenAPI operation.
+ */
 export const createOpenApiHandler = <OP extends Operation, SC extends number = 200>(handler: HttpHandler<any, OP['responses'][SC]['content']['application/json']>): APIGatewayv1Handler => {
   return createHttpHandler(handler);
 };
 
+/**
+ * A factory function that creates an HTTP request handler.
+ */
 export const createHttpHandler =
   <T, R>(handler: HttpHandler<T, R>): APIGatewayv1Handler => {
     return async (event, context) => {
+      // Create an object to hold the context for this HTTP request
       const ctx: HttpHandlerContext = {
         event,
         lambdaContext: context,
@@ -65,15 +96,21 @@ export const createHttpHandler =
         response: { headers: {}, json: true },
         cognitoAuth: new ApiGatewayv1CognitoAuthorizer(event, logger as unknown as logger.LambdaLog),
       };
+      // Add the request ID to the logging metadata, and enable debug logging if the DEBUG environment variable is set to "true"
       ctx.logger.options.meta.requestId = context.awsRequestId;
       ctx.logger.options.debug = process.env.DEBUG === 'true';
 
+      // Log the event object for debugging purposes
       ctx.logger.debug(JSON.stringify(event));
 
       try {
+        // Authenticate the user using the Cognito authorizer
         await ctx.cognitoAuth.authenticate();
 
+        // Call the user-defined handler function, passing in the request context object and the parsed request body
         const res = await handler(ctx, parseBody(event));
+
+        // Construct and return the HTTP response
         return {
           statusCode: ctx.response.statusCode ?? (res ? 200 : 204),
           headers: {
@@ -84,6 +121,9 @@ export const createHttpHandler =
           body: res ? (ctx.response.json ? JSON.stringify(res) : res) : '',
         };
       } catch (error: any) {
+        // Handle any errors that occur during request processing
+
+        // If the error is an instance of HttpError, return an HTTP response with the error status code and message
         if (error instanceof errors.HttpError) {
           return {
             statusCode: error.statusCode,
@@ -95,6 +135,7 @@ export const createHttpHandler =
             body: error.message,
           };
         }
+        // If the error is a Boom error (from the hapi.js framework), return an HTTP response with the error status code and payload
         if (error.isBoom) {
           return {
             statusCode: error.output.statusCode,
@@ -107,6 +148,7 @@ export const createHttpHandler =
             body: JSON.stringify(error.output.payload),
           };
         }
+        // For all other types of errors, log the error and return a 500 status code with the error message
         ctx.logger.error(error);
         return {
           statusCode: ctx.response.statusCode ?? 500,
@@ -121,24 +163,44 @@ export const createHttpHandler =
     };
   };
 
+/**
+ * This function takes an event object representing an API Gateway Proxy event
+ * with a Cognito authorizer, and extracts and parses the request body, if present.
+ *
+ * @param event The API Gateway Proxy event with a Cognito authorizer
+ * @returns The parsed request body
+ */
 function parseBody<T>(event: AWSLambda.APIGatewayProxyWithCognitoAuthorizerEvent): T {
   let body = event.body;
+
+  // If the request body is base64-encoded, decode it
   if (event.body && event.isBase64Encoded) {
     const buff = Buffer.from(event.body!, 'base64');
     body = buff.toString('utf8');
   }
+
+  // If the request body is in JSON format, parse it into a JavaScript object
   if (event.headers && event.headers['content-type']?.includes('application/json')) {
     return JSON.parse(body ?? '{}');
   }
+
+  // If the request body is not present or not in JSON format, return it as-is
   return body as any;
 }
 
+
+/**
+ * Returns the CORS headers to be included in the API Gateway response.
+ *
+ * @param event - The API Gateway event object.
+ * @returns An object containing the CORS headers.
+ */
 function corsHeader(event: AWSLambda.APIGatewayProxyWithCognitoAuthorizerEvent): { [name: string]: string } {
   return {
-    'Access-Control-Allow-Origin': event?.headers?.origin ?? '*',
-    'Access-Control-Allow-Credentials': event?.headers?.origin ? 'true' : 'false',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': 'Authorization, *',
+    'Access-Control-Allow-Origin': event?.headers?.origin ?? '*', // Allow requests from the origin header, or allow any origin if not present
+    'Access-Control-Allow-Credentials': event?.headers?.origin ? 'true' : 'false', // Include cookies in cross-origin requests if the origin header is present
+    'Access-Control-Allow-Methods': '*', // Allow any HTTP method
+    'Access-Control-Allow-Headers': 'Authorization, *', // Allow the Authorization header and any other headers
   };
 }
 
@@ -146,6 +208,10 @@ function corsHeader(event: AWSLambda.APIGatewayProxyWithCognitoAuthorizerEvent):
 /// AppSync
 /////////////////////////////////
 
+/**
+ * The context for the AppSync resolver function.
+ * Contains information about the event, lambda context, logger, and Cognito authorizer.
+ */
 export interface AppSyncHandlerContext<T> {
   event: AWSLambda.AppSyncResolverEvent<T>;
   lambdaContext: AWSLambda.Context;
@@ -153,10 +219,20 @@ export interface AppSyncHandlerContext<T> {
   cognitoAuth: CognitoAuthorizer;
 }
 
+/**
+ * A function that handles an AppSync resolver event.
+ * Receives the context object and returns the response.
+ */
 export type AppSyncHandler<T, R> = (
   context: AppSyncHandlerContext<T>,
 ) => Promise<R>;
 
+/**
+ * Creates an AppSync resolver handler from the given AppSyncHandler.
+ * Returns a function that can be used as an AppSync resolver.
+ *
+ * @param handler The AppSyncHandler to use as the resolver.
+ */
 export const createAppSyncHandler =
   <T, R>(handler: AppSyncHandler<T, R>): AWSLambda.AppSyncResolverHandler<T, R> => {
     return async (event, context) => {
@@ -166,15 +242,22 @@ export const createAppSyncHandler =
         logger: logger as unknown as logger.LambdaLog,
         cognitoAuth: new AppSyncCognitoAuthorizer(event),
       };
+
+      // set the meta information in logger
       ctx.logger.options.meta.requestId = context.awsRequestId;
+      // set the debug flag for logger
       ctx.logger.options.debug = process.env.DEBUG === 'true';
 
+      // log the received event
       ctx.logger.debug(JSON.stringify(event));
 
       try {
+        // authenticate the incoming request
         await ctx.cognitoAuth.authenticate();
+        // invoke the handler function with the context
         return await handler(ctx);
       } catch (error: any) {
+        // log the error and throw it again
         ctx.logger.error(error);
         throw error;
       }
